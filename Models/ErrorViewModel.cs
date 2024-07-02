231d9e09606b45847d3441d465155243ab0ec91e
meetvariya24 <150138468+meetvariya24@users.noreply.github.com>
2024-07-02 12:39:18 +0530

    fixed untracked files

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LAKSHYAEXIM.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
